04aae3f30aefa6b069a1d0f11e778754586c40b4
spyderpz <116122@student.drenthecollege.nl>
2019-01-11 15:15:10 +0100

    Working loging system and more mapping

--- controller/php/user.php
+++ controller/php/user.php
@@ -1,26 +1,38 @@
 <?php
 require_once("../../model/php/core.php");
-class user{
+class user
+{
 
-    function login($email,$wachtwoord){
+    function login($email, $wachtwoord)
+    {
         global $pdo;
         $usernamequery = $pdo->prepare("SELECT Email FROM personen WHERE Email = :email");
         $usernamequery->execute(['email' => $email]);
         $userres = $usernamequery->fetch();
-        if($userres){
+        if ($userres) {
             $passquery = $pdo->prepare("SELECT * FROM personen WHERE Wachtwoord = :password AND Email = :email");
-            $passquery->execute(['password' => $wachtwoord,'email' => $email]);
+            $passquery->execute(['password' => $wachtwoord, 'email' => $email]);
             $userres = $passquery->fetch();
-            if($userres){
+            if ($userres) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
-        }else{
+        } else {
             echo false;
         }
     }
-    function register($voornaam,$achternaam,$wachtwoord,$email,$postcode,$woonplaats){
-
+    function register($voornaam, $achternaam, $wachtwoord, $email, $postcode, $woonplaats){
+        global $pdo;
+        $availabilityquery = $pdo->prepare("SELECT Email FROM personen WHERE Email = :email");
+        $availabilityquery->execute(['email' => $email]);
+        $userres = $availabilityquery->fetch();
+        if ($userres) {
+            return false;
+        }else{
+            $newpersquery = $pdo->prepare("INSERT INTO personen(Voornaam,Achternaam,Wachtwoord,Email,Postcode,Woonplaats)VALUES (:Voornaam,:Achternaam,:Wachtwoord,:Email,:Postcode,:Woonplaats)");
+            $correct = $newpersquery->execute(['Voornaam' => $voornaam, 'Achternaam' => $achternaam, 'Wachwoord' => $wachtwoord, 'Email' => $email, 'Postcode' => $postcode, 'Woonplaats' => $woonplaats]);
+            return $correct;
+        }
     }
 }
