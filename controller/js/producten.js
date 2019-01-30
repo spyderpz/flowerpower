aba9909337dcae00ccd157f6411d57c9cccfef11
yarno <minejarno@gmail.com>
2019-01-30 11:20:11 +0100

    overlay styling

--- controller/js/producten.js
+++ controller/js/producten.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       var img = $(this).find('.panel-body').html();
       var description = $(this).find('.panel-footer').text();
 
-      $('.single-product').html('<div class="single-wrapper"><div class="title-single">'+title+'</div><div class="image">'+img+'</div><div class="description-single">'+description+'</div></div>');
+      $('.single-product').html('<div class="center"><div class="single-wrapper row"><div class="col-4"><div class="title-single">'+title+'</div><hr><div class="image">'+img+'</div></div><div class="col-6"><div class="description-single">'+description+'<button type="button">Click Me!</button></div></div></div></div>');
       $('.single-product').show();
       $('#overlay').show();
     });
