5959cb896cce4aa643f41f5e2411ddd3c7c4a5e0
yarno <minejarno@gmail.com>
2019-01-31 08:57:11 +0100

    image not found

--- view/php/producten.php
+++ view/php/producten.php
@@ -2,12 +2,13 @@
   $customCss = "<link rel='stylesheet' href='../../view/css/producten.css'>";
   $customTitle = "Producten";
   require_once("../../view/php/partials/_header.php");
-  $customScripts = "<script src='../../controller/js/producten.js' charset='utf-8'></script>";
+  $customScripts = "<script src='../../controller/js/producten.js' charset='utf-8'></script><script type='text/javascript'>function imgError(image){image.onerror = '';image.src = '../../model/img/bloemen/not-found.jpg';return true;}</script>";
   require_once("../../view/php/partials/_menu.php");
   require_once("../../controller/php/product.php");
   $products = new product();
   $productarray = $products->getproducts();
 ?>
+
 <div class="single-product">
 
 </div>
@@ -20,7 +21,7 @@
                 <div class='col-sm-4 product-wrapper' id='".$product['id']."'>
                     <div class='panel panel-primary'>
                         <div class='panel-heading'>".$product['Productnaam'].'<div class="productprijs">Prijs:'.$product['Prijs']."</div></div>
-                        <div class='panel-body'><img src='".$product['Image']."' class='img-responsive' style='width:100%' alt='Image'></div>
+                        <div class='panel-body'><img src='".$product['Image']."' class='img-responsive' style='width:100%' alt='Image' onerror='imgError(this);'></div>
                         <hr>
                         <div class='panel-footer '>".$product['Omschrijving']."</div>
                     </div>
