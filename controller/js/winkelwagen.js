42e17b8dcbb1207c0f8a4e2915eb8d29cd59a141
yarno <minejarno@gmail.com>
2019-02-01 10:33:13 +0100

    redirect fix

--- controller/js/winkelwagen.js
+++ controller/js/winkelwagen.js
@@ -29,7 +29,7 @@ $(document).ready( function(){
                 data: {bestel: true, ophaaldatum: orderdate,ophaallocatie:ophaallocatie,totprice: totprijs},
                 success: function (data) {
                     if(data){
-                        location.href = 'http://localhost/home/flowerpower/view/php/bestellingen.php';
+                        location.href = '../../view/php/bestellingen.php';
                     }else{
 
                     }
